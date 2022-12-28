616e858592651dc8272ee2cc00c7216e32d473a4
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2022-12-28 18:44:10 -0300

    Matching function for ActionTriggerCtrl
    - Added matching function for ActionTriggerCtrl::reset

--- src/xlink2ActionTriggerCtrl.cpp
+++ src/xlink2ActionTriggerCtrl.cpp
@@ -2,14 +2,23 @@
 
 namespace xlink2 {
 
-// NON_MATCHING?: two sub intructions reordered
-ActionTriggerCtrl::ActionTriggerCtrl(UserInstance* param_1, sead::Buffer<ModelTriggerConnection>* param_2, ResActionSlot* param_3) {
-    _0 = 0;
+ActionTriggerCtrl::ActionTriggerCtrl(UserInstance* param_1,
+                                     sead::Buffer<ModelTriggerConnection>* param_2,
+                                     ResActionSlot* param_3)
+{
     mBuffer = param_2;
-    mResActionSlot = param_3;
     mUserInstance = param_1;
+    mResActionSlot = param_3;
+    _0 = 0;
     _1 = 0;
     _2 = 0;
-    // ptr = nullptr;
-};
+    _3 = 0;
 }
+
+void ActionTriggerCtrl::reset() {
+    _2 = 0;
+    _0 = 0;
+    _1 = 0;
+    _3 = 0;
+}
+}  // namespace xlink2
