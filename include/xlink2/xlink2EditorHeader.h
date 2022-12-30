c0974ed21cd597500e15fb471ae2ec65c389a841
Nitr4m12 <10702988-Nitr4m12@users.noreply.gitlab.com>
2022-12-30 11:27:18 -0300

    Add EditorHeader

--- include/xlink2/xlink2EditorHeader.h
+++ include/xlink2/xlink2EditorHeader.h
@@ -1,5 +1,25 @@
 #pragma once
 
+#include "basis/seadTypes.h"
 namespace xlink2 {
-class EditorHeader {};
-}
+struct EditorHeader {
+    // Size = 0x44
+    u8 unknown[4];
+    u32 numResParam;
+    u32 numResAssetParam;
+    u32 triggerOverwriteParamTablePos;
+    u32 localPropertyNameRefTablePos;
+    u32 numResTriggerOverwriteParam;
+    u32 numLocalPropertyNameRefTable;
+    u32 numLocalPropertyEnumNameRefTable;
+    u32 numDirectValueTable;
+    u32 numRandomTable;
+    u32 numCurveTable;
+    u32 numCurvePointTable;
+    u32 exDataRegionPos;
+    u32 userNamePos;
+    u32 userBinPos;
+    u32 conditionTablePos;
+    u32 nameTablePos;
+};
+}  // namespace xlink2
