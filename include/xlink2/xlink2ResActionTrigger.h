64cdc14d8cb8b9c46f1d32808a0364cf6903d582
Nitr4m12 <10702988-Nitr4m12@users.noreply.gitlab.com>
2022-12-30 11:31:14 -0300

    Modify ResActionTrigger to be more accurate

--- include/xlink2/xlink2ResActionTrigger.h
+++ include/xlink2/xlink2ResActionTrigger.h
@@ -4,12 +4,12 @@
 
 namespace xlink2 {
 struct ResActionTrigger {
-    u32 guId;
+    s32 guId;
     u32 assetCtbPos;
-    u32 startFrame;
-    u32 endFrame;
+    s32 startFrame;
+    s32 endFrame;
     u16 flag;
-    u16 overwriteHash;
-    s32 overwriteParamPos;
+    s16 overwriteHash;
+    u32 overwriteParamPos;
 };
 }  // namespace xlink2
