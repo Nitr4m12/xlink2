f76b25d9055d4547d901dbb705239ed2b650ffc6
Nitr4m12 <10702988-Nitr4m12@users.noreply.gitlab.com>
2022-12-30 11:32:01 -0300

    Modify ResAssetCallTable to be more accurate

--- include/xlink2/xlink2ResAssetCallTable.h
+++ include/xlink2/xlink2ResAssetCallTable.h
@@ -5,15 +5,15 @@
 namespace xlink2 {
 struct ResAssetCallTable {
     u32 keyNamePos;
-    u16 assetId;
+    s16 assetId;
     u16 flag;
     s32 duration;
-    u32 parentIndex;
+    s32 parentIndex;
     u16 enumIndex;
     u8 isSolved;
     u8 unknown;
     u32 keyNameHash;
-    s32 paramStartPos;
-    s32 conditionPos;
+    u32 paramStartPos;
+    u32 conditionPos;
 };
 }
