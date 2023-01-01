0b8433cc1203c8dde77a68c02ac03620dde83e70
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-01 12:59:36 -0300

    AssetExecutorSLink: layout general structure

--- include/xlink2/xlink2AssetExecutorSLink.h
+++ include/xlink2/xlink2AssetExecutorSLink.h
@@ -4,8 +4,13 @@
 #include <prim/seadSafeString.h>
 
 #include "xlink2/xlink2AssetExecutor.h"
+#include "xlink2/xlink2EventSLink.h"
+#include "xlink2/xlink2UserInstanceSLink.h"
+#include "xlink2/xlink2ResAssetCallTable.h"
+#include "xlink2/xlink2ResTriggerOverwriteParam.h"
 
 namespace xlink2 {
+class EventSLink;
 class AssetExecutorSLink : AssetExecutor {
      ~AssetExecutorSLink() override;
 public:
@@ -38,17 +43,33 @@ public:
     void updateParam();
     void updateParamFirst();
 private:
-    // offset 0x18
-    // EventSLink* mEventSLink;
-
-    // offset 0x20
-    // UserInstanceSLink* mUserInstance;
-
-    // offset 0x38
-    // ResAssetCallTable* mResAssetCallTable;
-
-    // offset 0x68
-    // aal::Handle* mHandle;
+    u64 _0;
+    u64 _1;
+    EventSLink* mEventSLink; // 0x18
+    UserInstanceSLink* mUserInstance; // 0x20
+    ResAssetCallTable* mResAssetCallTable; // 0x28
+    f32 _2; // 0x30
+    f32 _3; // 0x34
+    ResTriggerOverwriteParam* mResTriggerOverwriteParam; // 0x38
+    void*  _4; // 0x40
+    u8 _5; //0x48
+    u8 _6[7]; // 0x49
+    u8 _7; // 0x50
+    u8 _8[7]; // 0x51
+    void* _9; // 0x58
+    void* _10; // 0x60
+    HandleSLink* mHandle; // 0x68
+    void* _11; // 0x70
+    u16 _12; // 0x78
+    u16 _13; // 0x7a
+    f32 mVolume; // 0x7c
+    f32 mDeviceVolume; // 0x80
+    f32 mPitch; // 0x84
+    f32 mLpf; // 0x88
+    f32 mPriority; // 0x8c
+    f32 mVelocityX; // 0x90
+    f32 mVelocityY; // 0x94
+    f32 mVelocityZ; // 0x98
 
 };
 }
