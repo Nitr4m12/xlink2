b96dd08558e2e389f3950a42634707bba89df663
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-02 18:50:35 -0300

    EventELink: layout possible structure
    - Members are commented as the full structure is not complete

--- include/xlink2/xlink2EventELink.h
+++ include/xlink2/xlink2EventELink.h
@@ -2,9 +2,12 @@
 
 #include "basis/seadTypes.h"
 
+#include "xlink2/xlink2Event.h"
+
 namespace xlink2 {
-class EventELink {
-    virtual ~EventELink();
+class EventELink : Event {
+     ~EventELink() override;
+
 public:
     class DelayEmitParam;
     void initializeImpl();
@@ -21,5 +24,22 @@ public:
 
     void setManualParticleEmission(bool);
     void setManualParticleEmissionWithParticleCount(int);
+private:
+    DelayEmitParam* mDelayEmitParam;
+
+    // 0x178
+    // bool
+
+    // 0x17c
+    // s32
+
+    // 0x180
+    // u32
+
+    // 0x184
+    // u32
+
+    // 0x190
+    // void*
 };
-}
+}  // namespace xlink2
