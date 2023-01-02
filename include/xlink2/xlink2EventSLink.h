bb12a010592d4bf481c235bff7bcf0d68ef9a597
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-02 18:51:01 -0300

    EventSLink: add member variables

--- include/xlink2/xlink2EventSLink.h
+++ include/xlink2/xlink2EventSLink.h
@@ -4,10 +4,13 @@
 #include <math/seadVectorFwd.h>
 
 #include "xlink2/xlink2AssetExecutorSLink.h"
+#include "xlink2/xlink2Event.h"
 
 namespace xlink2 {
-class EventSLink {
-    virtual ~EventSLink();
+class AssetExecutorSLink;
+class EventSLink : Event {
+     ~EventSLink() override;
+
 public:
     void initializeImpl();
 
@@ -15,7 +18,33 @@ public:
     u64 getAliveAssetExecutor(sead::PtrArray<AssetExecutorSLink>*) const;
     u64 getPos(sead::Vector3f*) const;
     // u64 getSourceHandle(sead::PtrArray<aal::Handle>*) const;
-    EventSLink* getVelocity() const;
+    u64 getVelocity() const;
+
 private:
+    u32 _16;
+    u32 _17;
+    u32 _18;
+    u32 _19;
+    void* _20;
+    u32 _21;
+    u32 _22;
+    u32 _23;
+    u32 _24;
+    u32 _25;
+    u32 _26;
+    u32 _27;
+    u32 _28;
+    void* _29;
+    void* _30;
+    u16 _31;
+
+    // 0xbc
+    // sead::Vector3f* _24;
+
+    // 0xc4
+    // u32
+
+    // 0xc8
+    // sead::Vector3f* mVelocity
 };
-}
+}  // namespace xlink2
