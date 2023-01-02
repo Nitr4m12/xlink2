3ca75913f2a6e0b78830eae95d0396a81aaff82c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-02 06:44:28 -0300

    ResourceAccessor2: add member variables

--- include/xlink2/xlink2ResourceAccessor2.h
+++ include/xlink2/xlink2ResourceAccessor2.h
@@ -4,6 +4,8 @@
 #include "xlink2/xlink2ResAssetCallTable.h"
 #include "xlink2/xlink2ResParam.h"
 #include "xlink2/xlink2ResRandomCallTable.h"
+#include "xlink2/xlink2UserInstance.h"
+#include "xlink2/xlink2UserResource.h"
 
 namespace xlink2 {
 class ResourceAccessor2 {
@@ -16,6 +18,13 @@ public:
     s64 getResRandomCallTable(ResParam const&) const;
 
 private:
+    UserInstance* mUserInstance;
+    UserResource* mUserResource;
     ResAssetCallTable* mResAssetCallTable;
+    void* _0;
+    u32 _1;
+    u8 _2;
+    void* _3;
+    User* mUser;
 };
-}
+}  // namespace xlink2
