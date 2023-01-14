23b21c017dfd0fb419df589f4b1242e4b882b032
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-14 05:09:01 -0300

    BlendContainer: add matching function
    Added matching function for "start()"

--- src/xlink2BlendContainer.cpp
+++ src/xlink2BlendContainer.cpp
@@ -1,6 +1,8 @@
 #include "xlink2/xlink2BlendContainer.h"
 
 namespace xlink2 {
+u8 BlendContainer::start() { return callAllChildContainer(); }
+
 void BlendContainer::initialize(Event* event, const ResAssetCallTable& res_asset_call_table) {
     ContainerBase::initialize(event, res_asset_call_table);
     ResourceUtil::getResContainerParam(mResAssetCallTable);
