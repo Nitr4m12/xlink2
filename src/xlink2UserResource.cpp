48845e2383c84779ab1b02ced59f0437261a712d
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-15 08:23:43 -0300

    UserResource: add matching function
    Matched UserResource::freeResourceParam_

--- src/xlink2UserResource.cpp
+++ src/xlink2UserResource.cpp
@@ -9,5 +9,23 @@ UserResource::UserResource(User* user) {
     _1 = nullptr;
 }
 
-void UserResource::onSetupResourceParam_(UserResourceParam*, const ParamDefineTable*, sead::Heap*) {}
+void UserResource::freeResourceParam_(UserResourceParam* param) {
+    if (param->nameTable != nullptr) {
+        delete[] param->nameTable;
+        param->nameTable = nullptr;
+        param->nameTableNum = 0;
+    }
+
+    if (param->_1 != nullptr) {
+        delete[] param->_1;
+        param->_1 = nullptr;
+        param->_0 = 0;
+    }
+
+    if (param->_3 != nullptr) {
+        delete[] param->_3;
+        param->_3 = nullptr;
+        param->_2 = 0;
+    }
+}
 }  // namespace xlink2
