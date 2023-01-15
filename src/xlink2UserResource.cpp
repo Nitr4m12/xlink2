e2570ba0bb7983eddc8a1a29be7ef66fbac5cbca
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-15 08:22:52 -0300

    UserResource: add matching constructor

--- src/xlink2UserResource.cpp
+++ src/xlink2UserResource.cpp
@@ -2,6 +2,12 @@
 
 namespace xlink2 {
 UserResource::~UserResource() = default;
+UserResource::UserResource(User* user) {
+    mUser = user;
+    mResMode = (ResMode)0;
+    mUserResourceParam = nullptr;
+    _1 = nullptr;
+}
 
 void UserResource::onSetupResourceParam_(UserResourceParam*, const ParamDefineTable*, sead::Heap*) {}
 }  // namespace xlink2
