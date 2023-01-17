0bb6f30b89688263368b907bc8e4073951489dcd
Nitr4m12 <10702988-Nitr4m12@users.noreply.gitlab.com>
2023-01-16 22:34:24 -0300

    UserResource: start work on getters

--- src/xlink2UserResource.cpp
+++ src/xlink2UserResource.cpp
@@ -28,4 +28,87 @@ void UserResource::freeResourceParam_(UserResourceParam* param) {
         param->_2 = 0;
     }
 }
+
+// TODO
+void* UserResource::getActionTriggerTableItem(s32 index) const {
+    UserResourceParam*const* param_ptr_ptr;
+    UserResourceParam* param_ptr;
+
+    // Adding an enum to a the address of a pointer? Maybe navigating through the class
+    param_ptr_ptr = &mUserResourceParam + (int)mResMode;
+    if (2 > (u32)mResMode) {
+        param_ptr_ptr = &mUserResourceParam;
+    }
+    param_ptr = *param_ptr_ptr;
+    if (param_ptr != nullptr) {
+        if (param_ptr->_7 != 0) {
+            if (-1 < index)
+                if (param_ptr->resUserHeader->numResActionTrigger <= index)
+                    return nullptr;
+            return param_ptr->directValueTable + index*0x18;
+        }
+    }
+    return nullptr;
+}
+
+// TODO
+void* UserResource::getAlwaysTriggerTableItem(s32 index) const {
+    UserResourceParam*const* param_ptr_ptr;
+    UserResourceParam* param_ptr;
+
+    param_ptr_ptr = &mUserResourceParam + (int)mResMode;
+    if (2 > (u32)mResMode) {
+        param_ptr_ptr = &mUserResourceParam;
+    }
+    param_ptr = *param_ptr_ptr;
+    if (param_ptr != nullptr) {
+        if (param_ptr->_7 != 0) {
+            if (-1 < index)
+                if (param_ptr->resUserHeader->numResAlwaysTrigger <= index)
+                    return nullptr;
+            return param_ptr->curvePointTable + index*0x10;
+        }
+    }
+    return nullptr;
+}
+
+// TODO
+void* UserResource::getAssetCallTableItem(s32 index) const {
+    UserResourceParam*const* param_ptr_ptr;
+    UserResourceParam* param_ptr;
+
+    param_ptr_ptr = &mUserResourceParam + (int)mResMode;
+    if (2 > (u32)mResMode) {
+        param_ptr_ptr = &mUserResourceParam;
+    }
+    param_ptr = *param_ptr_ptr;
+    if (param_ptr != nullptr) {
+        if (param_ptr->_7 != 0) {
+            if (-1 < index)
+                if (param_ptr->resUserHeader->numCallTable <= (long)index)
+                    return nullptr;
+            return param_ptr->resAssetParamTable + index*0x20;
+        }
+    }
+    return nullptr;
+}
+
+// TODO
+ResUserHeader* UserResource::getUserHeader() const {
+    UserResourceParam*const* param_ptr_ptr;
+    UserResourceParam* param_ptr;
+
+    param_ptr_ptr = ResMode::Editor > mResMode ? &mUserResourceParam : &mUserResourceParam + (s32)mResMode;
+    param_ptr = *param_ptr_ptr;
+    if (param_ptr != nullptr) {
+        if (param_ptr->_7 != 0) {
+            return param_ptr->resUserHeader;
+        }
+    }
+    return nullptr;
+}
+
+void UserResource::checkAndAddErrorMultipleKeyByTrigger(const ResAssetCallTable&, TriggerType) {}
+void UserResource::onSetupResourceParam_(UserResourceParam*, const ParamDefineTable*, sead::Heap*) {
+}
 }  // namespace xlink2
