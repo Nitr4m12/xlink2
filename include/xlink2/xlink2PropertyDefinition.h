2a5107e0c2f9dcaeb26ee4435b7b84d3c7d25a0d
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-21 08:28:48 -0300

    PropertyDefinition: added members

--- include/xlink2/xlink2PropertyDefinition.h
+++ include/xlink2/xlink2PropertyDefinition.h
@@ -1,8 +1,20 @@
 #pragma once
 
-#include "basis/seadTypes.h"
+#include <basis/seadTypes.h>
+#include <prim/seadSafeString.h>
+
+#include "xlink2/xlink2PropertyType.h"
+
 namespace xlink2 {
-struct PropertyDefinition {
-    u32 namePos;
+class PropertyDefinition {
+public:
+    virtual ~PropertyDefinition();
+
+protected:
+    void* _0;
+    sead::FixedSafeString<64>* mUserName;
+    u8 unknown[70];
+    PropertyType mType;
+    u8 _1;
 };
 }  // namespace xlink2
