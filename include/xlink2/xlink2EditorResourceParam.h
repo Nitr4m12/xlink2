9b705fcff0136407030c74a69455bcc6591a8e8e
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-21 08:35:20 -0300

    EditorResourceParam: add members

--- include/xlink2/xlink2EditorResourceParam.h
+++ include/xlink2/xlink2EditorResourceParam.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#include "prim/seadSafeString.h"
+#include "xlink2/xlink2CommonResourceParam.h"
+#include "xlink2/xlink2EditorHeader.h"
 namespace xlink2 {
-class EditorResourceParam {};
+struct EditorResourceParam : CommonResourceParam {
+    EditorHeader* pEditorHeader;
+    u32 _0;
+    ResUserHeader* pResUserHeader2;
+    sead::SafeString* editorName;
+    ResUserHeader* pResUserHeader3;
+    s32 unknownNum;
+
+};
 }  // namespace xlink2
