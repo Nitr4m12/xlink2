b89a5e5718d5c0c9b60fd0e589e34244d92bdd64
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-01-25 17:35:19 -0300

    ContainerBase: add most function definitions
    Most still need to be checked for a matching status

--- src/xlink2BlendContainer.cpp
+++ src/xlink2BlendContainer.cpp
@@ -3,8 +3,8 @@
 namespace xlink2 {
 bool BlendContainer::start() { return callAllChildContainer(); }
 
-void BlendContainer::initialize(Event* event, const ResAssetCallTable& res_asset_call_table) {
-    ContainerBase::initialize(event, res_asset_call_table);
-    ResourceUtil::getResContainerParam(mResAssetCallTable);
-}
+// void BlendContainer::initialize(Event* event, const ResAssetCallTable& res_asset_call_table) {
+//     ContainerBase::initialize(event, res_asset_call_table);
+//     ResourceUtil::getResContainerParam(mResAssetCallTable);
+// }
 }  // namespace xlink2
--- src/xlink2ContainerBase.cpp
+++ src/xlink2ContainerBase.cpp
@@ -1,4 +1,5 @@
 #include "xlink2/xlink2ContainerBase.h"
+#include "xlink2/xlink2ContainerCreator.h"
 
 namespace xlink2 {
 ContainerBase::ContainerBase() {
@@ -7,5 +8,56 @@ ContainerBase::ContainerBase() {
     mChildContainers[1] = nullptr;
     mResAssetCallTable = nullptr;
     mEvent = nullptr;
+}
+
+void* ContainerBase::createChildContainer_(ResAssetCallTable const& asset_call_table,
+                                           ContainerBase* container) {
+    auto* child_container{ContainerCreator::CreateContainer(mEvent, asset_call_table)};
+
+    if (!child_container)
+        return nullptr;
+
+    bool started{child_container->start()};
+    if (!(started & 1)) {
+        child_container->destroy();
+        child_container = nullptr;
+    } else if (container)
+        mChildContainers[0] = child_container;
+    else
+        container->mChildContainers[1] = child_container;
+
+    return child_container;
+}
+
+void ContainerBase::fade(int p1) {
+    for (auto* child_container : mChildContainers) {
+        child_container->fade(p1);
+    }
+
+    mResAssetDuration = 0;
+}
+
+void ContainerBase::fadeBySystem() {
+    for (auto* child_container : mChildContainers) {
+        child_container->fadeBySystem();
+    }
+
+    mResAssetDuration = 0;
+}
+
+// NON-MATCHING: one extra subroutine
+void ContainerBase::initialize(Event* event, const ResAssetCallTable& asset_call_table) {
+    mResAssetCallTable = &asset_call_table;
+    mEvent = event;
+    event->getUserInstance()->getUser()->getUserResource()->getAccessor();
+    mResAssetDuration = asset_call_table.duration;
+}
+
+void ContainerBase::kill() {
+    for (auto* child_container : mChildContainers) {
+        child_container->kill();
+    }
+
+    mResAssetDuration = 0;
 }
 }  // namespace xlink2
