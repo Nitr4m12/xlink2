2a347cfa67d79c6fe89e37ec96a1a9001fa03f2f
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-05-14 21:00:33 -0400

    UserInstance: added some function definitions
    Matching:
    - UserInstance::changeAction
    - UserInstance::changeInstanceParam
    - UserInstance::doOtameshiEmit_
    - UserInstance::getContainerTypeName
    - UserInstance::isCurrentActionNeedToCalc
    - UserInstance::isCurrentActionNeedToObserve
    
    WIP:
    - UserInstance::checkAndErrorCallWithoutSetup_
    - UserInstance::clearAllEvent

--- src/xlink2UserInstance.cpp
+++ src/xlink2UserInstance.cpp
@@ -1,14 +1,68 @@
 #include <xlink2/xlink2UserInstance.h>
 
 namespace xlink2 {
+void UserInstance::changeAction(char const* name, int p1, int p2) {
+    auto* sys = mUser->getSystem();
+    if (sys->getCallEnable())
+        mTriggerCtrlMgr.changeAction(name, p1, p2);
+}
+
+void UserInstance::changeAction(int p1, int p2, int p3) {
+    auto* sys = mUser->getSystem();
+    if (sys->getCallEnable())
+        mTriggerCtrlMgr.changeAction(p1, p2, p3);
+}
+
+void UserInstance::changeInstanceParam(ResMode mode) {
+    u8 _6var = _0xD0;
+    if (mode != ResMode::Editor)
+        _6var &= 0xfe;
+    else
+        _6var |= 1;
+    _0xD0 = _6var;
+    mTriggerCtrlMgr.setResMode(mode);
+}
+
+void UserInstance::checkAndBreakWhenEmit_(const char* /*unused*/) {}
+
 u64 UserInstance::checkAndErrorCallInCalc(const char* /*unused*/, ...) const {
     return 1;
+}
+
+// WIP
+u64 UserInstance::checkAndErrorCallWithoutSetup_(const char* p1, ...) const {
+    if (!mParamsByResMode[_0xD0 & 1] || !mParamsByResMode[_0xD0 & 1]) {
+        sead::BufferedSafeString d(nullptr, 100);
+        d.formatV(p1, std::va_list());
+        mUser->getSystem()->addError(Error::Type(0x19), mUser, "%s");
+        return 0;
+    }
+    return 1;
+}
+
+// WIP
+void UserInstance::clearAllEvent() {
+    System* sys = mUser->getSystem();
+    auto* lock_obj = sys->getModuleLockObj();
+    lock_obj->lock();
+    if (mEventList.begin() != mEventList.end()) {
+    }
 }
 
 void UserInstance::doEventActivatedCallback_(const Locator& /*unused*/, Event* /*unused*/) {}
 
 u32 UserInstance::doEventActivatingCallback_(const Locator& /*unused*/) {
     return 0;
+}
+
+void UserInstance::doOtameshiEmit_() {};
+
+char* UserInstance::getContainerTypeName(ResAssetCallTable const& asset_call) const {
+    return mUser->getUserResource()->getAccessor()->getCallTableTypeName(asset_call);
+}
+
+u64 UserInstance::getCurrentResActionIdx(s32 index) const {
+    return mTriggerCtrlMgr.getCurrentResActionIdx(index);
 }
 
 u32 UserInstance::getDefaultGroup() const {
@@ -19,9 +73,17 @@ char* UserInstance::getUserName() const {
     return mUser->getUserName();
 }
 
-// bool UserInstance::isCurrentActionNeedToCalc() const {
-//     return _15 != 0;
-// }
+bool UserInstance::isCurrentActionNeedToCalc() const {
+    return mTriggerCtrlMgr.get1() != 0;
+}
+
+bool UserInstance::isCurrentActionNeedToObserve(s32 index) const {
+    if (mUser->getSystem()->getCallEnable())
+        return mTriggerCtrlMgr.isCurrentActionNeedToObserve(index);
+
+    return false;
+}
+
 
 bool UserInstance::isDebugLogEnable(DebugLogFlag /*unused*/) const {
     return false;
