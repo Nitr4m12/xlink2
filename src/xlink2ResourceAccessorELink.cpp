7371b83b8575351809a14c9f26f15800753d6467
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-10-21 21:42:11 -0300

    ResourceAccessorELink: Fixed wrong names

--- src/xlink2ResourceAccessorELink.cpp
+++ src/xlink2ResourceAccessorELink.cpp
@@ -3,23 +3,23 @@
 
 namespace xlink2 {
 f32 ResourceAccessorELink::getOverwriteAlpha(u32 p1, const UserInstance* p2) const {
-    return ResourceAccessor::getResOverwriteParamValueFloat(p1, 0x16, p2);
+    return ResourceAccessor::getResOverwriteParamValueFloat_(p1, 0x16, p2);
 }
 
 char* ResourceAccessorELink::getOverwriteBoneName(u32 p1) const {
-    return ResourceAccessor::getResOverwriteParamValueString(p1, 0xb);
+    return ResourceAccessor::getResOverwriteParamValueString_(p1, 0xb);
 }
 
 f32 ResourceAccessorELink::getOverwriteColorBlue(u32 p1, const UserInstance* p2) const {
-    return ResourceAccessor::getResOverwriteParamValueFloat(p1, 0x15, p2);
+    return ResourceAccessor::getResOverwriteParamValueFloat_(p1, 0x15, p2);
 }
 
 f32 ResourceAccessorELink::getOverwriteColorGreen(u32 p1, const UserInstance* p2) const {
-    return ResourceAccessor::getResOverwriteParamValueFloat(p1, 0x14, p2);
+    return ResourceAccessor::getResOverwriteParamValueFloat_(p1, 0x14, p2);
 }
 
 f32 ResourceAccessorELink::getOverwriteColorRed(u32 p1, const UserInstance* p2) const {
-    return ResourceAccessor::getResOverwriteParamValueFloat(p1, 0x13, p2);
+    return ResourceAccessor::getResOverwriteParamValueFloat_(p1, 0x13, p2);
 }
 
 bool ResourceAccessorELink::isBoneNameOverwritten(u32 p1) const {
