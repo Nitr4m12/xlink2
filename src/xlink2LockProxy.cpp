f25e70048514cccf221de5e07716345e6d517f4e
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2023-10-22 18:07:30 -0300

    LockProxy: added definitions for non-specialization

--- src/xlink2LockProxy.cpp
+++ src/xlink2LockProxy.cpp
@@ -1,7 +1,28 @@
 #include "xlink2/xlink2LockProxy.h"
 
 namespace xlink2 {
-LockProxy<sead::SpinLock>::~LockProxy() = default;
-void LockProxy<sead::SpinLock>::lock() { mLock.lock(); }
-void LockProxy<sead::SpinLock>::unlock() { mLock.unlock(); }
+template <typename T>
+LockProxy<T>::~LockProxy() = default;
+
+template <typename T>
+void LockProxy<T>::lock() {
+    mLock.lock();
 }
+
+template <typename T>
+void LockProxy<T>::unlock() {
+    mLock.unlock();
+}
+
+
+LockProxy<sead::SpinLock>::~LockProxy() = default;
+
+void LockProxy<sead::SpinLock>::lock() {
+    mLock.lock();
+};
+
+void LockProxy<sead::SpinLock>::unlock() {
+    mLock.unlock();
+};
+
+}  // namespace xlink2
