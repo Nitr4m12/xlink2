dc458009d5cf2b22ce09840a827f86ed6d518918
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2025-06-25 19:20:42 -0400

    ResourceAccessorELink: matched getEmissionScale

--- src/xlink2ResourceAccessorELink.cpp
+++ src/xlink2ResourceAccessorELink.cpp
@@ -162,6 +162,11 @@ f32 ResourceAccessorELink::getEmissionRate(const ResAssetCallTable& asset_ctb, c
     return getResParamValueFloat("getEmissionRate", asset_ctb, 23, 1.0, user_instance);
 }
 
+f32 ResourceAccessorELink::getEmissionScale(const ResAssetCallTable& asset_ctb, const UserInstance* user_instance) const
+{
+    return getResParamValueFloat("getEmissionScale", asset_ctb, 24, 1.0, user_instance);
+}
+
 f32 ResourceAccessorELink::getOverwriteAlpha(u32 p1, const UserInstance* p2) const {
     return ResourceAccessor::getResOverwriteParamValueFloat_(p1, 0x16, p2);
 }
