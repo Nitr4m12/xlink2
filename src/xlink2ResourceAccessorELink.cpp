d4156a0cb471602009d60cfc6f367c7c245ab1e8
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2025-06-25 20:04:07 -0400

    ResourceAccessorELink: matched getOverwriteRotationY

--- src/xlink2ResourceAccessorELink.cpp
+++ src/xlink2ResourceAccessorELink.cpp
@@ -216,6 +216,11 @@ f32 ResourceAccessorELink::getOverwriteRotationX(u32 idx, const UserInstance* us
 {
     return this->getResOverwriteParamValueFloat_(idx, 16, user_instance);
 }
+
+f32 ResourceAccessorELink::getOverwriteRotationY(u32 idx, const UserInstance* user_instance) const
+{
+    return this->getResOverwriteParamValueFloat_(idx, 17, user_instance);
+}
 }
 
 f32 ResourceAccessorELink::getOverwriteColorRed(u32 p1, const UserInstance* p2) const {
