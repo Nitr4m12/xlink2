b1a7e5509247c0c4bb965c04aae3e58e26ba5fe2
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2025-06-25 21:16:08 -0400

    ResourceAccessorSLink: matched getPitchWithOverwrite

--- src/xlink2ResourceAccessorSLink.cpp
+++ src/xlink2ResourceAccessorSLink.cpp
@@ -177,6 +177,14 @@ f32 ResourceAccessorSLink::getVolumeWithOverwrite(const ResAssetCallTable& asset
     return getVolume(asset_ctb, user_instance);
 }
 
+f32 ResourceAccessorSLink::getPitchWithOverwrite(const ResAssetCallTable& asset_ctb, u32 idx, const UserInstance* user_instance) const
+{
+    if (this->isParamOverwritten(idx, 6))
+        return getOverwritePitch(idx, user_instance);
+
+    return getPitch(asset_ctb, user_instance);
+}
+
 bool ResourceAccessorSLink::isBoneNameOverwritten(u32 p1) const {
     return isParamOverwritten(p1, 0xf);
 }
