2ac43e7618f28a1c5515c61c118f042641e7512b
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2025-07-11 21:55:48 -0400

    ResourceBuffer: matched searchResUserHeader

--- src/xlink2ResourceBuffer.cpp
+++ src/xlink2ResourceBuffer.cpp
@@ -1,5 +1,8 @@
 #include <xlink2/xlink2ResourceBuffer.h>
+#include "codec/seadHashCRC32.h"
+#include "xlink2/xlink2ResUserHeader.h"
 #include "xlink2/xlink2ResourceParamCreator.h"
+#include "xlink2/xlink2Util.h"
 
 namespace xlink2 {
 ResourceBuffer::~ResourceBuffer() = default;
@@ -9,6 +12,44 @@ void ResourceBuffer::applyGlobalPropertyDefinition(System* system) {
         ResourceParamCreator::solveAboutGlobalProperty(&mRomResourceParam, &mParamDefineTable,
                                                               system);
     }
+}
+
+ResUserHeader* ResourceBuffer::searchResUserHeader(const char* user_name) const
+{
+    if (mRomResourceParam.isInitialized && mRomResourceParam.numUser != 0) {
+        // Based on binarySearch_ from seadSharcArchiveRes.cpp
+
+        u32 hash {sead::HashCRC32::calcStringHash(user_name)};
+
+        u32 middle;
+        
+        u32 end = mRomResourceParam.numUser;
+        u32 start {0};
+
+        while (true) {
+            middle = (start + end) / 2;
+
+            u32 entry_hash {mRomResourceParam.nameHashTable[middle]};
+
+            if (entry_hash == hash)
+                return calcOffset<ResUserHeader>(mRomResourceParam.offsetTable[middle]);
+
+            if (entry_hash < hash) {
+                if (start == middle)
+                    return nullptr;
+    
+                start = middle;
+            }
+            else {
+                if (end == middle)
+                    return nullptr;
+
+                end = middle;
+            }
+        }
+    }
+
+    return nullptr;
 }
 
 RomResourceParam ResourceBuffer::getEmptyRomResourceParam() {
