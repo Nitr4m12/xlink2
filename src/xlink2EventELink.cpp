13391ebdf1c711fbc7ceacf07bcc01184f96826d
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-03-19 15:25:13 -0300

    EventELink: matched callEventDestroyCallback_

--- src/xlink2EventELink.cpp
+++ src/xlink2EventELink.cpp
@@ -13,10 +13,26 @@ void EventELink::callEventCreateCallback_()
         system_callback->eventCreate(arg);
     }
 
-    auto* instance_callback {user_instance->getEventCallback()};
-    if (instance_callback != nullptr) {
+    auto* user_instance_callback {user_instance->getEventCallback()};
+    if (user_instance_callback != nullptr) {
         IEventCallbackELink::EventArg arg {mpAssetCallTable, user_instance, nullptr, nullptr, this};
-        instance_callback->eventCreate(arg);
+        user_instance_callback->eventCreate(arg);
+    }
+}
+
+void EventELink::callEventDestroyCallback_()
+{
+    auto* system_callback {SystemELink::instance()->getEventCallback()};
+    auto* user_instance {getUserInstanceELink()};
+    if (system_callback != nullptr) {
+        IEventCallbackELink::EventArg arg {mpAssetCallTable, user_instance, nullptr, nullptr, this};
+        system_callback->eventDestroy(arg);
+    }
+
+    auto* user_instance_callback {user_instance->getEventCallback()};
+    if (user_instance_callback != nullptr) {
+        IEventCallbackELink::EventArg arg {mpAssetCallTable, user_instance, nullptr, nullptr, this};
+        user_instance_callback->eventDestroy(arg);
     }
 }
 } // namespace xlink2
