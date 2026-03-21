f85dc9d094e5bf93279cc21a32462383cbc5f104
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-03-21 00:46:04 -0300

    UserInstanceSLink: matched emit(const Locator&, HandleSLink*)

--- src/xlink2UserInstanceSLink.cpp
+++ src/xlink2UserInstanceSLink.cpp
@@ -8,4 +8,14 @@
 
 namespace xlink2 {
 UserInstanceSLink::~UserInstanceSLink() = default;
+
+void UserInstanceSLink::searchAndEmit(const char* asset_key_name, HandleSLink* handle)
+{
+    searchAndEmitImpl(asset_key_name, handle);
+}
+
+void UserInstanceSLink::emit(const Locator& locator, HandleSLink* handle)
+{
+    emitImpl(locator, handle);
+}
 }  // namespace xlink2
