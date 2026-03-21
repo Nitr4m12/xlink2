d9b9b5eb543360cb3726b9ee2dcd6fc35b0ed7c8
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-03-21 00:35:11 -0300

    UserInstanceParam: added ELink and SLink variants

--- include/xlink2/xlink2UserInstanceParam.h
+++ include/xlink2/xlink2UserInstanceParam.h
@@ -5,6 +5,10 @@
 
 #include "xlink2/xlink2ModelAssetConnection.h"
 
+namespace aal {
+class Emitter;
+}
+
 namespace xlink2 {
 struct UserInstanceParam {
     struct RandomHistory {
@@ -17,5 +21,14 @@ struct UserInstanceParam {
     bool isSetupRom {false};
 };
 static_assert(sizeof(UserInstanceParam) == 0x28, "xlink2::UserInstanceParam size mismatch");
+
+struct UserInstanceParamELink : UserInstanceParam {};
+static_assert(sizeof(UserInstanceParamELink) == 0x28, "xlink2::UserInstanceParamELink size mismatch");
+
+struct UserInstanceParamSLink : UserInstanceParam {
+    sead::Buffer<aal::Emitter> emitterBuffer1;
+    sead::Buffer<aal::Emitter> emitterBuffer2;
+};
+static_assert(sizeof(UserInstanceParamSLink) == 0x48, "xlink2::UserInstanceParamSLink size mismatch");
 
 }  // namespace xlink2
