b97866808b713912f5fc7c64bf370625e553981f
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-03-25 14:35:17 -0300

    MonoContainer: matched kill

--- src/xlink2MonoContainer.cpp
+++ src/xlink2MonoContainer.cpp
@@ -1,4 +1,6 @@
 #include "xlink2/xlink2MonoContainer.h"
+#include "xlink2/xlink2AssetExecutor.h"
+#include "xlink2/xlink2Event.h"
 
 namespace xlink2 {
 MonoContainer::~MonoContainer() = default;
@@ -11,4 +13,19 @@ bool MonoContainer::killOneTimeEvent()
     }
     return false;
 }
+
+void MonoContainer::kill()
+{
+    if (mpChild != nullptr) {
+        auto* event {mpEvent};
+        auto* child_executor {reinterpret_cast<AssetExecutor*>(mpChild)};
+        mpChild = nullptr;
+        event->getAliveAssetExecutors().erase(child_executor);
+        event->getFadeBySystemExecutors().pushBack(child_executor);
+        child_executor->kill();
+    }
+
+    mAssetDuration = 0;
+    _1 = -1.0f;
 }
+} // namespace xlink2
