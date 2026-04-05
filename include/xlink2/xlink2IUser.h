8c765fe3c632e8a43840f1746a1a3fd3bd2a2a2c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-04-05 09:27:07 -0400

    IUser: matched `getMtxCorrectingDrawBone`

--- include/xlink2/xlink2IUser.h
+++ include/xlink2/xlink2IUser.h
@@ -8,20 +8,20 @@ namespace xlink2 {
 class IUser {
 public:
     virtual sead::Matrix34f* getBoneWorldMtxPtr(const char*) const = 0;
-    virtual char* getActionSlotName(s32 action_slot_idx) const;
+    virtual const char* getActionSlotName([[maybe_unused]] s32 action_slot_idx) const;
 
     virtual void* getDebugDrawCamera() const;
     virtual void* getDebugDrawProjection() const;
     virtual char* getDebugUserName() const;
 
     virtual const char* getUserInformation() const;
-    virtual void getReservedAssetName(ToolConnectionContext*) const;
+    virtual void getReservedAssetName([[maybe_unused]] ToolConnectionContext* ctx) const;
 
     virtual u32 getNumBone() const = 0;
     virtual const char* getBoneName(s32 bone_idx) const = 0;
 
-    virtual u32 getNumAction(int) const;
-    virtual char* getActionName(int, int) const;
+    virtual u32 getNumAction([[maybe_unused]] s32) const;
+    virtual char* getActionName([[maybe_unused]] s32, [[maybe_unused]] s32) const;
 
     virtual void captureScreen(const char*) = 0;
 
@@ -30,6 +30,6 @@ public:
     virtual sead::Matrix34f* getAutoInputMtxSource() const = 0;
 
     void getBoneWorldMtx(const char*, sead::Matrix34f*) const;
-    virtual const sead::Matrix34f* getMtxCorrectingDrawBone() const;
+    virtual sead::Matrix34f getMtxCorrectingDrawBone() const;
 };
 }  // namespace xlink2
--- src/xlink2IUser.cpp
+++ src/xlink2IUser.cpp
@@ -1,7 +1,7 @@
 #include "xlink2/xlink2IUser.h"
 
 namespace xlink2 {
-char* IUser::getActionSlotName(int) const
+const char* IUser::getActionSlotName([[maybe_unused]] s32 action_slot_idx) const
 {
     return nullptr;
 }
@@ -26,16 +26,20 @@ const char* IUser::getUserInformation() const
     return nullptr;
 }
 
-void IUser::getReservedAssetName(ToolConnectionContext*) const {}
+void IUser::getReservedAssetName([[maybe_unused]] ToolConnectionContext* ctx) const {}
 
-u32 IUser::getNumAction(int) const
+u32 IUser::getNumAction([[maybe_unused]] s32) const
 {
     return 0;
 }
 
-char* IUser::getActionName(int, int) const
+char* IUser::getActionName([[maybe_unused]] s32, [[maybe_unused]] s32) const
 {
     return nullptr;
 }
 
+sead::Matrix34f IUser::getMtxCorrectingDrawBone() const
+{
+    return sead::Matrix34f::ident;
+}
 }  // namespace xlink2
